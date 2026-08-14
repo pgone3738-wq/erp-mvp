edccbda1ae7755eb4082ce79add980205b98b2c1
pgone3738-wq <pgone3738@gmaile.com>
2026-08-14 17:08:11 +0300

    Fix CORS for Vercel

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,6 +3,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT || 3000, '0.0.0.0');
+  app.enableCors({
+    origin: '*', // Allow requests from Vercel!
+    credentials: true,
+  });
+  const port = process.env.PORT || 3000; 
+  await app.listen(port);
 }
 bootstrap();
